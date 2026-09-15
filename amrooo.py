261b93a6ef9deae337e0f878b17e67c5b39c9d1a
Amro Tarawneh <amro.tarawneh04@gmail.com>
2026-09-15 13:06:18 +0300

    Add amrooo.py

---
+++ amrooo.py
@@ -0,0 +1,35 @@
+S = "Hello there!"
+
+print(S)
+
+print(S[0:6])      
+
+print(S[7])        
+
+print(S[-1])       
+
+print(S[-2])       
+
+print(S[4:])       
+
+print(S[-5:])      
+S= ['apple', 'orange', 'peach']
+type(S)
+print(S[0])
+print(S[0:2])
+print(S[-1])
+print(S[0:-2])
+print(S[-2])
+print(S[-3:0])
+print ("apple"in S)
+print ("aPple"in S)
+age =35
+print(age)
+print(type(age))
+print(f"I am {age} years old")
+print("happy".upper())
+print(S[0].upper())
+for i in range(1,10):
+    print(i)
+    print("OK ... done!")
+
